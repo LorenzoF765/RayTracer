427d6aa51441c315d491e50ec82466b62fb8596b
LorenzoF765 <100639060+LorenzoF765@users.noreply.github.com>
2022-11-30 17:27:58 -0700

    Ray Tracer Assignment

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+#include"Renderer.h"
+#include<iostream>
+int main(int,char**){
+	Renderer renderer;
+	renderer.Initialize();
+	renderer.CreateWindow(400,300);
+	bool quit=false;
+	while(!quit)
+	{
+		SDL_Event event;
+		SDL_PollEvent(&event);
+		switch(event.type)
+		{
+		caseSDL_QUIT:quit=true;break;
+		}
+	}
+	renderer.Shutdown();
+	return 0;
 }
