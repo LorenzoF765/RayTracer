57e9a6eae7422c8384f6ed79873e08b14b635d85
LorenzoF765 <100639060+LorenzoF765@users.noreply.github.com>
2022-12-04 22:41:27 -0700

    Renderer Assignment done

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -1,9 +1,14 @@
 #include"Renderer.h"
 #include<iostream>
+
+
 int main(int,char**){
 	Renderer renderer;
 	renderer.Initialize();
-	renderer.CreateWindow(400,300);
+	renderer.CreateWindow(420,690);
+
+	Canvas canvas(420, 690, renderer);
+
 	bool quit=false;
 	while(!quit)
 	{
@@ -11,8 +16,34 @@ int main(int,char**){
 		SDL_PollEvent(&event);
 		switch(event.type)
 		{
-		caseSDL_QUIT:quit=true;break;
+		case SDL_QUIT:
+			quit=true;
+			break;
+		case SDL_KEYDOWN:
+			switch (event.key.keysym.sym)
+			{
+			case SDLK_ESCAPE:
+				quit = true;
+				break;
+			}
+			break;
 		}
+		//canvas.Clear({ 0, 0, 0, 1 });
+		renderer.Render(canvas);
+		//canvas.Update();
+		
+		
+		//for (int i = 0; i < 26746; i++) {
+
+		//	canvas.DrawPoint({ random(0, 420), random(0, 690) }, { 1, 1, 1, 1 });
+		//	canvas.DrawPoint({ random(0, 420), random(0, 690) }, { 0, 0, 1, 1 });
+		//	canvas.DrawPoint({ random(0, 420), random(0, 690) }, { 1, 0, 0, 1 });
+		//	canvas.DrawPoint({ random(0, 420), random(0, 690) }, { 0, 1, 0, 1 });
+		//}
+		canvas.Update();
+
+		renderer.CopyCanvas(canvas);
+		renderer.Present();
 	}
 	renderer.Shutdown();
 	return 0;
--- Renderer.h
+++ Renderer.h
@@ -1,12 +1,23 @@
 #pragma once
 #include<SDL.h>
+#include "Canvas.h"
+#include "Ray.h"
 class Renderer{ 
 public:
 	Renderer() = default; 
 	bool Initialize(); 
 	void Shutdown(); 
 	bool CreateWindow(int width,int height); 
+
+	void CopyCanvas(const Canvas& canvas);
+	void Present();
+
+	friend class Canvas;
+
+	void Render(Canvas& canvas);
+
 private:
+	color3 GetBackgroundFromRay(const Ray& ray);
 	SDL_Window * m_window{nullptr}; 
 	   SDL_Renderer * m_renderer{nullptr}; 
 };
